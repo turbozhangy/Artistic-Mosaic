8ba1cfc8e0f24555147af06dfe7dc170bdd85fa3
wpp5354 <wpp5354@vip.qq.com>
2014-11-24 02:39:41 +0800

    IntroductionDemo更新
    IntroductionDemo更新

--- Source/IntroductionSite/js/main.js
+++ Source/IntroductionSite/js/main.js
@@ -9,4 +9,9 @@ $(function() {
 		e.preventDefault();
 		$.scrollify("move",$(this).attr("href"));
 	});
+	
+	var top = ($(window).height()-62);
+	var left = ($(window).width()-32)/2;
+	$("#jiantou").css({'margin-top': top+'px', 'margin-left': left+'px'})
+	
 });
