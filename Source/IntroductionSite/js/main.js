3a7cd822a5776b1c5347626bb3d9c435bef3868f
wpp5354 <wpp5354@vip.qq.com>
2014-11-26 23:48:03 +0800

    更新

--- Source/IntroductionSite/js/main.js
+++ Source/IntroductionSite/js/main.js
@@ -13,5 +13,18 @@ $(function() {
 	var top = ($(window).height()-62);
 	var left = ($(window).width()-32)/2;
 	$("#jiantou").css({'margin-top': top+'px', 'margin-left': left+'px'})
+	$("#jiantou").hover(function(){
+		$(this).addClass('jiantou');
+		$(this).removeClass('jiantou_1')
+	},function(){
+		$(this).removeClass('jiantou')
+		$(this).addClass('jiantou_1');
+	});
 	
+	$("#jiantou").bind("click",function(){
+		
+		$('body,html').animate({scrollTop:window.pageYOffset+$(window).height()},1000);
+		//alert(window.pageYOffset);
+		//alert($(window).height());
+	});
 });
