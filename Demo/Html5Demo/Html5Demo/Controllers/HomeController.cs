6bf5ca9a658d5b1cd39c854e042568c7cf007a37
zhangyanglogin <331033463@qq.com>
2014-12-03 22:28:41 +0800

    上传使用html5构建蒙太奇效果的代码

---
+++ Demo/Html5Demo/Html5Demo/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿/************************************************************************************
+ * Copyright (c) $year$ ZhangYang All Rights Reserved.
+ * CLR版本： $clrversion$
+ * 命名空间：$rootnamespace$
+ * 文件名：  $safeitemname$.cs
+ * 创建人：  ZhangYang
+ * 电子邮箱：zhangyanglogin@gmail.com
+ * 创建时间：$time$
+ * 描述：
+ ************************************************************************************/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Html5Demo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
