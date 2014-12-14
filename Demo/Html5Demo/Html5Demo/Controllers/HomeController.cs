486d38d7be51528c792c31908439dd12f02875c4
zhangyanglogin <331033463@qq.com>
2014-12-14 21:48:07 +0800

    javascript生成图片

--- Demo/Html5Demo/Html5Demo/Controllers/HomeController.cs
+++ Demo/Html5Demo/Html5Demo/Controllers/HomeController.cs
@@ -11,8 +11,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Aspose.Imaging;
 
 namespace Html5Demo.Controllers
 {
