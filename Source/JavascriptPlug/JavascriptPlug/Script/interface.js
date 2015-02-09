d0bec122956cb8e818b3d454f7c2acca0d59b60a
ZhangY <zhangyanglogin@gmail.com>
2015-02-09 19:05:40 +0800

    保存图片
    自己实现canvas保存图片

--- Source/JavascriptPlug/JavascriptPlug/Script/interface.js
+++ Source/JavascriptPlug/JavascriptPlug/Script/interface.js
@@ -14,38 +14,53 @@ function init() {
             amImgArray.push(imgs[i]);
         }
     }
-    
-    var iframe = document.createElement("iframe");
-    iframe.style.width = "100%";
-    iframe.style.height = "100%";
-    iframe.src = "http://localhost:50226";
-    iframe.onload = function () {
-        debugger;
+    //判断页面是否有Iframe
+    var iframes = document.getElementsByTagName("iframe");//
+    if (iframes.length > 0) {
+        for (var i = 0; i < iframes.length; i++) {
+            var imgs = iframes[i].contentWindow.document.getElementsByTagName("img");
+            if (imgs.length > 0)
+            {
+                for (var i = 0; i < imgs.length; i++) {
+                    ////筛选,原始宽高大于100的图片
+                    if (imgs[i].naturalWidth > 100 && imgs[i].naturalHeight > 100) {
+                        amImgArray.push(imgs[i]);
+                    }
+                }
+            }
+        }
     }
-    //var warp = document.createElement('div');
-    //warp.id = "am_pulg";
-    //warp.style.width = "100%";
-    //warp.style.height = "100%";
-    //warp.style.background = "#fff";
-
-
-
     
-
-    //var oScript = document.createElement("script");
-    //oScript.type = "text/javascript";
-    //oScript.onload = function () {
-    //  // var am = new AM("http://localhost:50226", "am_pulg");
-    //  // am.init(amImgArray);
-    //};
-    //oScript.src = "http://localhost:50226/Script/am.js";
-    
-    //iframe.appendChild(oScript);
-
+    //var iframe = document.createElement("iframe");
+    //iframe.style.width = "100%";
+    //iframe.style.height = "100%";
+    //iframe.src = "http://localhost:50226";
+    //iframe.onload = function () {
+    //   // var s = iframe.contentWindow.document;
+    //    debugger;
+    //}
+    var warp = document.createElement('div');
+    warp.id = "am_pulg";
+    warp.style.width = "100%";
+    warp.style.height = "100%";
+    warp.style.background = "#fff";
     if (document.getElementById('am_pulg') == null) {
         document.body.innerHTML = "";
-        document.body.appendChild(iframe);
+        document.body.appendChild(warp);
     }
+    
+
+    var oScript = document.createElement("script");
+    oScript.type = "text/javascript";
+    oScript.onload = function () {
+       var am = new AM("http://localhost:50226", "am_pulg");
+       am.init(amImgArray);
+    };
+    oScript.src = "http://localhost:50226/Script/am.js";
+    
+    document.body.appendChild(oScript);
+
+
     //.appendChild(oScript);
 };
 init();
